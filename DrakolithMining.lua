d189fb9d1cc3f79f9408104a0b1bed566c26b81d
notHartley <166396306+notHartley@users.noreply.github.com>
2024-04-08 17:16:15 -0600

    Update DrakolithMining.lua
    adjustments to banking method

--- DrakolithMining.lua
+++ DrakolithMining.lua
@@ -7,8 +7,6 @@ local startTime = os.time()
 local startXp = API.GetSkillXP("MINING")
 local afk = os.time() -- Initialize the variable afk
 local MAX_IDLE_TIME_MINUTES = 15 -- Define the maximum idle time in minutes
-local fillOreBoxTimer = os.time()
-
 
 -- Constant modules
 -- Define the function to round numbers
@@ -75,8 +73,6 @@ local function calcProgressPercentage(skill, currentExp)
     return math.floor(progressPercentage)
 end
 
-local oreBoxFillCounter = 0  -- Initialize the ore box fill counter
-
 local function printProgressReport(final)
     local skill = "MINING"
     local currentXp = API.GetSkillXP(skill)
@@ -86,18 +82,12 @@ local function printProgressReport(final)
     local time = formatElapsedTime(startTime)
     local currentLevel = API.XPLevelTable(API.GetSkillXP(skill))
     IGP.radius = calcProgressPercentage(skill, API.GetSkillXP(skill)) / 100
-    
-    -- Calculate remaining time until the ore box needs to be filled
-    local remainingTime = 240 - (os.difftime(os.time(), fillOreBoxTimer))
-    local remainingMinutes = math.floor(remainingTime / 60)
-    local remainingSeconds = remainingTime % 60
-    
     IGP.string_value = time ..
             " | " ..
             string.lower(skill):gsub("^%l", string.upper) ..
-            ": " .. currentLevel .. " | XP/H: " .. formatNumber(xpPH) .. " | XP: " .. formatNumber(diffXp) ..
-            " | Ore Box Fill: " .. oreBoxFillCounter .. "/5" .. " | Next Fill In: " .. remainingMinutes .. "m " .. remainingSeconds .. "s"  -- Add ore box fill counter and next fill timer
+            ": " .. currentLevel .. " | XP/H: " .. formatNumber(xpPH) .. " | XP: " .. formatNumber(diffXp)
 end
+
 
 local function setupGUI()
     IGP = API.CreateIG_answer()
@@ -114,9 +104,7 @@ end
 setupGUI()
 
 -- Variable modules
-local drakolith = {113071, 113072, 113073}  -- IDs of Drakolith ores
-local fillOreBoxTimer = os.time()  -- Initialize the ore box fill timer
-local bankingCounter = 0  -- Initialize the banking counter
+local drakolith = {113071, 113072, 113073}  -- IDs of Drakolith rocks
 
 -- Define the function to shuffle a table
 local function shuffle(tbl)
@@ -198,7 +186,7 @@ local function EnterDungeon()
     API.WaitUntilMovingandAnimEnds()
     API.RandomSleep2(5000, 8050, 12000) -- Add sleep after climbing the rocks outside the dungeon
     print("Accessing mining dungeon...")
-    API.RandomSleep2(5000, 8050, 12000)
+    API.RandomSleep2(5000, 8050, 12000) -- Add sleep after climbing the rocks outside the dungeon
     API.DoAction_Object1(0x39, 0, {52860}, 50)
     API.WaitUntilMovingandAnimEnds()
     print("Returned to dungeon from banking.")
@@ -214,9 +202,13 @@ end
 -- Define the function to fill the ore box
 local function FillOreBox()
     print("Attempting to fill ore box...")
-    API.DoAction_Interface(0x24, 0xaef9, 1, 1473, 5, 0, 3808)
-    API.WaitUntilMovingandAnimEnds()
-    print("Successfully filled ore box.")
+    if UTILS.getAmountInOrebox(UTILS.ORES.DRAKOLITH) < 120 then
+        API.DoAction_Interface(0x24, 0xaef9, 1, 1473, 5, 0, 3808)
+        API.WaitUntilMovingandAnimEnds()
+        print("Successfully filled ore box.")
+    else
+        print("Ore box is already full.")
+    end
 end
 
 -- Define the function to mine rocks
@@ -225,21 +217,22 @@ local function MineRock(rockID)
     API.WaitUntilMovingandAnimEnds()
 end
 
+local function updateProgressReport()
+    while true do
+        printProgressReport()
+        API.RandomSleep2(1000, 1100, 1200)  -- Sleep for approximately 1 second before updating again
+    end
+end
+
+-- Start the coroutine
+local progressCoroutine = coroutine.create(updateProgressReport)
+
 -- Main loop environment
 API.Write_LoopyLoop(true)
 while(API.Read_LoopyLoop()) do
     drawGUI()
     idleCheck()
-    printProgressReport()
     API.DoRandomEvents()
-
-    -- Check if it's time to fill the ore box
-    if os.difftime(os.time(), fillOreBoxTimer) >= 240 then
-        FillOreBox()
-        oreBoxFillCounter = oreBoxFillCounter + 1
-        fillOreBoxTimer = os.time()  -- Update the fillOreBoxTimer after filling the ore box
-    end
-    end
 
     if scriptJustInitiated then
         VarrockTeleport()
@@ -247,58 +240,63 @@ while(API.Read_LoopyLoop()) do
         EnterDungeon()
         scriptJustInitiated = false  -- Update scriptJustInitiated after the initial setup
     else
-        if API.Invfreecount_() > 0 then
-            print("idle check")
-            if not API.IsPlayerAnimating_(player, 3) then
-                API.RandomSleep2(1500, 6050, 2000)        
-                if not API.IsPlayerAnimating_(player, 2) then
-                    print("idle so start mining...")
-                    -- Shuffle the ore IDs
-                    drakolith = shuffle(drakolith)
-                    -- Mine the first ore in the shuffled list
-                    MineRock(drakolith[1])
-                    -- Wait until the mining animation ends before proceeding
-                    API.WaitUntilMovingandAnimEnds()
-
-                    -- Check for sparkling rocks while mining is idle
-                    local foundSparkling = API.FindHl(0x3a, 0, drakolith, 50, { 7165, 7164 })
-                    if foundSparkling then
-                        print("Sparkle found")
-                        MineRock(foundSparkling)  -- Mine the sparkling rock
-                    end
-                end
+        if API.InvFull_() then
+            if UTILS.getAmountInOrebox(UTILS.ORES.DRAKOLITH) < 120 then
+                FillOreBox()
+            else
+                GETeleport()
+                Banker()
+                BankItems()
+                VarrockTeleport()
+                WalkSome()
+                EnterDungeon()
             end
         else
-            print(API.LocalPlayer_HoverProgress())
-            while API.LocalPlayer_HoverProgress() <= 90 do
-                print("no stamina..")
+            if API.Invfreecount_() > 0 then
+                print("idle check")
+                if not API.IsPlayerAnimating_(player, 3) then
+                    API.RandomSleep2(1500, 6050, 2000)        
+                    if not API.IsPlayerAnimating_(player, 2) then
+                        print("idle so start mining...")
+                        -- Shuffle the ore IDs
+                        drakolith = shuffle(drakolith)
+                        -- Mine the first ore in the shuffled list
+                        MineRock(drakolith[1])
+                        -- Wait until the mining animation ends before proceeding
+                        API.WaitUntilMovingandAnimEnds()
+
+                        -- Check for sparkling rocks while mining is idle
+                        local foundSparkling = API.FindHl(0x3a, 0, drakolith, 50, { 7165, 7164 })
+                        if foundSparkling then
+                            print("Sparkle found")
+                            MineRock(foundSparkling)  -- Mine the sparkling rock
+                        end
+                    end
+                end
+            else
                 print(API.LocalPlayer_HoverProgress())
-                -- Try to find and mine a sparkling rock
-                local foundSparkling = API.FindHl(0x3a, 0, drakolith, 50, { 7165, 7164 })
-                if  foundSparkling then
-                    print("Sparkle found")
-                    MineRock(foundSparkling)  -- Mine the sparkling rock
-                    API.RandomSleep2(2500, 3050, 12000)
-                else
-                    -- If no sparkling rock was found, mine the first ore in the shuffled list
-                    print("No Sparkle found")
-                    MineRock(drakolith[1])
-                    API.RandomSleep2(2500, 3050, 12000)
+                while API.LocalPlayer_HoverProgress() <= 90 do
+                    print("no stamina..")
+                    print(API.LocalPlayer_HoverProgress())
+                    -- Try to find and mine a sparkling rock
+                    local foundSparkling = API.FindHl(0x3a, 0, drakolith, 50, { 7165, 7164 })
+                    if  foundSparkling then
+                        print("Sparkle found")
+                        MineRock(foundSparkling)  -- Mine the sparkling rock
+                        API.RandomSleep2(2500, 3050, 12000)
+                    else
+                        -- If no sparkling rock was found, mine the first ore in the shuffled list
+                        print("No Sparkle found")
+                        MineRock(drakolith[1])
+                        API.RandomSleep2(2500, 3050, 12000)
+                    end
                 end
             end
         end
     end
 
-    -- Check if the ore box fill counter has reached 5 before teleporting and banking
-    if oreBoxFillCounter >= 5 then
-        oreBoxFillCounter = 0
-        GETeleport()
-        Banker()
-        BankItems()
-        VarrockTeleport()
-        WalkSome()
-        EnterDungeon()
-    end
-
     API.RandomSleep2(500, 3050, 12000)
+
+    -- Resume the coroutine to update the progress report
+    coroutine.resume(progressCoroutine)
 end
